e81c5d217f5fea2e3f3ef8c592b500f34a9d4f2a
jowrites <jodie.keene@torontofilmschool.ca>
2024-09-30 17:24:11 -0400

    slime testing
    so slimy

--- SlimeWorld/Assets/Scripts/Slime/DocileSlime.cs
+++ SlimeWorld/Assets/Scripts/Slime/DocileSlime.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class DocileSlime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Slime Movement Properties")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float squishAmount = 0.2f;
+    [SerializeField] private float squishSpeed = 5f;
+
+    private Vector3 originalScale;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        originalScale = transform.localScale;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Basic movement logic
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Calculate movement direction relative to gravity
+        Vector3 gravityDirection = (GravitySource.instance.transform.position - transform.position).normalized;
+        Vector3 right = Vector3.Cross(gravityDirection, transform.forward).normalized;
+        Vector3 forward = Vector3.Cross(right, gravityDirection).normalized;
+
+        Vector3 movement = (right * horizontal + forward * vertical).normalized * speed;
+
+        rb.velocity = movement + rb.velocity.y * gravityDirection;
+
+        if (movement != Vector3.zero)
+        {
+            Vector3 targetScale = new Vector3(
+                originalScale.x - Mathf.Abs(movement.x) * squishAmount,
+                originalScale.y + squishAmount,
+                originalScale.z - Mathf.Abs(movement.z) * squishAmount
+                );
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * squishSpeed);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime * squishSpeed);
+        }
+
     }
+
 }
